08df5ee3adfc20f6004927f10658f1ded4a5dfa7
oolst71 <115809707+oolst71@users.noreply.github.com>
2022-12-02 11:43:03 +0100

    created new class for obstacle

---
+++ Source/Obstacle.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "raylib.h"
+
+class Obstacle {
+private:
+	//Private Variables
+
+	//Screen Variables
+
+public:
+	//Public Variables
+
+private:
+	//Private Functions
+
+public:
+	//Public Functions
+
+};
